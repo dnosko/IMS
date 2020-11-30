fb4f5ebac5f401067559e8d71a474548b391a68a
dnosko <dasa.noskova@gmail.com>
2020-11-30 16:03:52 +0100

    oil init

--- automata.py
+++ automata.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 class Automata:
+
+    max_concentration = 10 #max number of oil particles
     
     def __init__(self,x,y):
         """ Inits grid of cellular automata to x,y shape """
@@ -9,10 +11,19 @@ class Automata:
         self.columns = y
 
     
+    def init_oil(self,coordinates):
+        """ Inits oil spill. Takes list of x1,y1,x2,y2 coordinates """
+        x1,y1,x2,y2 = coordinates
+        
+        for x in range(x1,x2+1):
+            for y in range(y1,y2+1):
+                self.grid[x,y] = self.max_concentration
+
+    
     def print_grid(self):
-        neighbors = self.get_neighbors([4,4])
-        for x,y in neighbors:
-            self.grid[x,y] = 1
+        #neighbors = self.get_neighbors([4,4])
+        #for x,y in neighbors:
+        #    self.grid[x,y] = 1
         print(self.grid)
 
 
@@ -33,9 +44,12 @@ class Automata:
         return neighbors
 
 
+    def nextGeneration(self):
+        pass
     
 
 
 if __name__ == "__main__":
     ca = Automata(5,5)
+    ca.init_oil([1,1,3,3])
     ca.print_grid()
